4320796e52a8e2604decfdc043c0ef5211e9e074
Ryan Feng <odayfans@gmail.com>
2014-07-10 17:54:06 -0400

    Add xcode project

---
+++ iMods/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iMods
+//
+//  Created by Ryan Feng on 6/10/14.
+//  Copyright (c) 2014 Ryan Feng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
