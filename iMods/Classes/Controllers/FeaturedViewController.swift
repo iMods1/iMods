13fecdfba62e550b1dfe50690eec0e5a204dc7b3
Brendon Roberto <brendon@brendonroberto.com>
2014-08-07 23:28:01 -0400

    Modify FeaturedViewController to use two UIButtons instead of customizing UISegmentedControl because it's simpler.

--- iMods/Classes/Controllers/FeaturedViewController.swift
+++ iMods/Classes/Controllers/FeaturedViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class FeaturedViewController: UIViewController {
 
-    @IBOutlet var tweaksThemesSegmentedControl: UISegmentedControl?
+    @IBOutlet var themesButton: UIButton!
+    @IBOutlet var tweaksButton: UIButton!
+    var featuredTableViewController: FeaturedTableViewController?
+    let tweaksDataSource = TweaksTestDataSource()
+    let themesDataSource = ThemesTestDataSource()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        themesButton.selected = true
+        tweaksButton.selected = false
+    }
+
+    @IBAction func themesButtonWasTouched(sender: UIButton) {
+        sender.selected = true
+        tweaksButton.selected = false
+
+        featuredTableViewController!.dataSource = themesDataSource
+        (featuredTableViewController!.view as UITableView).reloadData()
+    }
+
+    @IBAction func tweaksButtonWasTouched(sender: UIButton) {
+        sender.selected = true
+        themesButton.selected = false
+
+        featuredTableViewController!.dataSource = tweaksDataSource
+        (featuredTableViewController!.view as UITableView).reloadData()
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        super.prepareForSegue(segue, sender: sender)
+        // Store the contained ViewController for later use
+        if segue.identifier == "featured_tableview_embed" {
+            self.featuredTableViewController = segue.destinationViewController as? FeaturedTableViewController
+        }
     }
 
     override func didReceiveMemoryWarning() {
