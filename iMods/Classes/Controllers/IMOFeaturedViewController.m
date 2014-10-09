2265e1e2a34109ad43a480ef1a71147e9622c34c
Brendon Roberto <brendon@brendonroberto.com>
2014-10-09 03:09:34 -0400

    Cleanup storyboard, add IBOutlet for bannerImage to IMOFeaturedViewController

--- iMods/Classes/Controllers/IMOFeaturedViewController.m
+++ iMods/Classes/Controllers/IMOFeaturedViewController.m
@@ -16,6 +16,7 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIButton *themesButton;
 @property (weak, nonatomic) IBOutlet UIButton *tweaksButton;
+@property (weak, nonatomic) IBOutlet UIImageView *bannerImage;
 
 @property (strong, nonatomic) NSArray *items;
 @property (strong, nonatomic) IMOItemManager *manager;
