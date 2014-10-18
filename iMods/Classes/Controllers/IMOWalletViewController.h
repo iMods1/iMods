221cd66acd8e59b518702ada72f0de236ace38eb
Brendon Roberto <brendon@brendonroberto.com>
2014-10-17 19:47:45 -0500

    Add IMOWalletViewController

---
+++ iMods/Classes/Controllers/IMOWalletViewController.h
@@ -0,0 +1,13 @@
+//
+//  IMOWalletViewController.h
+//  iMods
+//
+//  Created by Brendon Roberto on 10/22/14.
+//  Copyright (c) 2014 Ryan Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMOWalletViewController : UIViewController
+
+@end
---
+++ iMods/Classes/Controllers/IMOWalletViewController.m
@@ -0,0 +1,37 @@
+//
+//  IMOWalletViewController.m
+//  iMods
+//
+//  Created by Brendon Roberto on 10/22/14.
+//  Copyright (c) 2014 Ryan Feng. All rights reserved.
+//
+
+#import "IMOWalletViewController.h"
+
+@interface IMOWalletViewController ()
+
+@end
+
+@implementation IMOWalletViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
